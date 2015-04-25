c7c1d231583bfbcb9173e5a6a1c333903613f785
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-25 17:03:54 -0500

    updated fetch

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 cd /data/www/nepal/json
 wget -r -nH --cut-dirs=1 --no-parent --reject="index.html*" http://ec2-54-242-150-21.compute-1.amazonaws.com/nepal/
+
+nodejs ~/osmdown/bin/osmdown build ~/nepal-earthquake/nepal.osmdown
