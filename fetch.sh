57eb301f14d39082d109fd79625948ccd37f54cf
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-26 13:58:02 -0600

    updating fetch

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
+echo "Getting latest from Mikel's exports"
 cd /data/www/nepal/json
 wget -r -nH --cut-dirs=1 --no-parent --reject="index.html*" http://ec2-54-242-150-21.compute-1.amazonaws.com/nepal/
 
+echo "Getting lateset update to nepal.osmdown from github"
+git pull
+
+echo "Building the page"
 nodejs ~/osmdown/bin/osmdown build ~/nepal-earthquake/nepal.osmdown
