6ab75f4c556d9ddbcf0a92b9ce38e375956540c8
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-29 23:49:52 -0600

    updating the live version

--- mongo_scripts/user_maintenance.js
+++ mongo_scripts/user_maintenance.js
@@ -1,6 +1,6 @@
 //Find duplicates: (Collection and then key)
 db.users.aggregate(
-    { $group : {_id : "$id", total : { $sum : 1 } } },
+    { $group : {_id : "$uid", total : { $sum : 1 } } },
     { $match : { total : { $gte : 2 } } },
     { $sort : {total : -1} },
     { $limit : 5 }
