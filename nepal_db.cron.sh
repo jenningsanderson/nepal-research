1f2dcbbf1216bb1a2b51befbd077770724bcda6e
Jennings Anderson <jennings.anderson@colorado.edu>
2015-05-01 02:05:53 -0600

    fixing it

--- nepal_db.cron.sh
+++ nepal_db.cron.sh
@@ -1,12 +1,12 @@
 date
 echo "Running Nepal_db_cron.sh"
-cd ~/epic-osm/
+cd /home/osm/epic-osm/
 
-rake realtime ~/nepal-earthquake/nepal-earthquake_rt.yml
+rake realtime /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 
 #Run the realtime updates
-rake import:osmtm_tags ~/nepal-earthquake/nepal-earthquake_rt.yml
-rake import:nodeways   ~/nepal-earthquake/nepal-earthquake_rt.yml
+rake import:osmtm_tags /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+rake import:nodeways   /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 
 #Ensure we have users for the entire analysis window
-rake import:users      ~/nepal-earthquake/nepal-earthquake.yml
+rake import:users      /home/osm/nepal-earthquake/nepal-earthquake.yml
--- nepal_questions.cron.sh
+++ nepal_questions.cron.sh
@@ -1,16 +1,16 @@
 date
 echo "Running Nepal_Questions_cron.sh"
-cd ~/epic-osm/
+cd /home/osm/epic-osm/
 
 #Update the analysis window to reflect the latest
-rake realtime ~/nepal-earthquake/nepal-earthquake_rt.yml
+rake realtime /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 
 #Run the time-binned questions
-rake questions:changesets  ~/nepal-earthquake/nepal-earthquake_rt.yml
-rake questions:users       ~/nepal-earthquake/nepal-earthquake_rt.yml
-rake questions:multi_users ~/nepal-earthquake/nepal-earthquake_rt.yml
+rake questions:changesets  /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+rake questions:users       /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+rake questions:multi_users /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 # rake questions:ways        ~/nepal-earthquake/nepal-earthquake_rt.yml
 
 #Run other questions?
-rake questions:users       ~/nepal-earthquake/nepal-earthquake.yml
-rake questions:changesets  ~/nepal-earthquake/nepal-earthquake.yml
+rake questions:users       /home/osm/nepal-earthquake/nepal-earthquake.yml
+rake questions:changesets  /home/osm/nepal-earthquake/nepal-earthquake.yml
