d489d447974bfef8fb3cb516ae272d0568b03670
Jennings Anderson <jennings.anderson@colorado.edu>
2015-05-01 02:16:12 -0600

    updating qustions

--- nepal_db.cron.sh
+++ nepal_db.cron.sh
@@ -2,11 +2,19 @@ date
 echo "Running Nepal_db_cron.sh"
 cd /home/osm/epic-osm/
 
-rake realtime /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+echo "Currently in: "
+pwd
 
+echo "Running the realtime update to the config file"
+# rake realtime /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+
+date
+echo "Running tag and nodeway imports"
 #Run the realtime updates
 rake import:osmtm_tags /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 rake import:nodeways   /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 
+date
+echo "Running User Imports"
 #Ensure we have users for the entire analysis window
 rake import:users      /home/osm/nepal-earthquake/nepal-earthquake.yml
--- nepal_questions.cron.sh
+++ nepal_questions.cron.sh
@@ -2,15 +2,21 @@ date
 echo "Running Nepal_Questions_cron.sh"
 cd /home/osm/epic-osm/
 
+echo "Currently in: "
+pwd
+
+echo "Updating the analysis window time"
 #Update the analysis window to reflect the latest
 rake realtime /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 
+echo "Running the time binned questions"
 #Run the time-binned questions
 rake questions:changesets  /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 rake questions:users       /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 rake questions:multi_users /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 # rake questions:ways        ~/nepal-earthquake/nepal-earthquake_rt.yml
 
+echo "Running all the other questions"
 #Run other questions?
 rake questions:users       /home/osm/nepal-earthquake/nepal-earthquake.yml
 rake questions:changesets  /home/osm/nepal-earthquake/nepal-earthquake.yml
