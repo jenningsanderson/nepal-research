d5e7a974b2cb39dcc0956b485cafdfcc6a14596a
Jennings Anderson <jennings.anderson@colorado.edu>
2015-05-01 02:18:27 -0600

    updating them

--- nepal_db.cron.sh
+++ nepal_db.cron.sh
@@ -6,15 +6,15 @@ echo "Currently in: "
 pwd
 
 echo "Running the realtime update to the config file"
-# rake realtime /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+/usr/local/rbenv/shims/rake realtime /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 
 date
 echo "Running tag and nodeway imports"
 #Run the realtime updates
-rake import:osmtm_tags /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
-rake import:nodeways   /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+/usr/local/rbenv/shims/rake import:osmtm_tags /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+/usr/local/rbenv/shims/rake import:nodeways   /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 
 date
 echo "Running User Imports"
 #Ensure we have users for the entire analysis window
-rake import:users      /home/osm/nepal-earthquake/nepal-earthquake.yml
+/usr/local/rbenv/shims/rake import:users      /home/osm/nepal-earthquake/nepal-earthquake.yml
--- nepal_questions.cron.sh
+++ nepal_questions.cron.sh
@@ -7,16 +7,16 @@ pwd
 
 echo "Updating the analysis window time"
 #Update the analysis window to reflect the latest
-rake realtime /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+/usr/local/rbenv/shims/rake realtime /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 
 echo "Running the time binned questions"
 #Run the time-binned questions
-rake questions:changesets  /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
-rake questions:users       /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
-rake questions:multi_users /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+/usr/local/rbenv/shims/rake questions:changesets  /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+/usr/local/rbenv/shims/rake questions:users       /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
+/usr/local/rbenv/shims/rake questions:multi_users /home/osm/nepal-earthquake/nepal-earthquake_rt.yml
 # rake questions:ways        ~/nepal-earthquake/nepal-earthquake_rt.yml
 
 echo "Running all the other questions"
 #Run other questions?
-rake questions:users       /home/osm/nepal-earthquake/nepal-earthquake.yml
-rake questions:changesets  /home/osm/nepal-earthquake/nepal-earthquake.yml
+/usr/local/rbenv/shims/rake questions:users       /home/osm/nepal-earthquake/nepal-earthquake.yml
+/usr/local/rbenv/shims/rake questions:changesets  /home/osm/nepal-earthquake/nepal-earthquake.yml
